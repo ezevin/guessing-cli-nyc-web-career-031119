e94dcdc9f8eab85c75b0d0cce49c58d8ac747cda
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-20 16:46:46 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,7 +1,7 @@
 # Code your solution here!
 def run_guessing_game
     puts  "Guess a number between 1 and 6."
-  guess = gets.downcase.chomp
+  guess = gets.chomp
   computer = rand(1..6).to_s
  if guess == computer 
    puts "You guessed the correct number!"
